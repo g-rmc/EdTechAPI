5f5f65b90ae1b52a3baae9335f1c29e2fa4323bb
g-rmc <g.rmc3000@gmail.com>
2023-03-17 02:44:44 -0300

    Start

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+//import coasterRouter from './routers/coaster.router.js';
+
+const app = express();
+
+app
+    .use(cors())
+    .use(express.json())
+    .get('/status', (_req, res) => res.send('Ok!'))
+    .get('/hello', (_req, res) => res.send('Hello!'))
+    //.use(coasterRouter);
+
+export default app;
